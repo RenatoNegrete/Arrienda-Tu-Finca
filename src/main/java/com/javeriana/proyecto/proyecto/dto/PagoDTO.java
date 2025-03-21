7ad525a78cea75a9f25ed5ebae3250b9279d5ee9
felo312 <149544556+felo312@users.noreply.github.com>
2025-03-20 20:53:16 -0500

    Update PagoDTO.java

---
+++ src/main/java/com/javeriana/proyecto/proyecto/dto/PagoDTO.java
@@ -0,0 +1,18 @@
+package com.javeriana.proyecto.proyecto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PagoDTO {
+
+    private long id;
+    private String numCuenta;
+    private int estado;
+
+}
