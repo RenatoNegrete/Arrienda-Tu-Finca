9dbff2a7e941543727f986dc44647c9646f85000
felo312 <149544556+felo312@users.noreply.github.com>
2025-03-20 20:47:07 -0500

    Update Banco.java

---
+++ src/main/java/com/javeriana/proyecto/proyecto/entidades/Banco.java
@@ -0,0 +1,30 @@
+package com.javeriana.proyecto.proyecto.entidades;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Where(clause = "status = 0")
+@SQLDelete(sql = "UPDATE administrador SET status = 1 WHERE id=?")
+
+public class Banco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private long id;
+    private String nombre;
+}
