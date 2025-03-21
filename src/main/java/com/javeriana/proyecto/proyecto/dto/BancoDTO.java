14c4a3dc63c57c1323750301dd7446af879f300e
felo312 <149544556+felo312@users.noreply.github.com>
2025-03-20 20:52:03 -0500

    Update BancoDTO.java

---
+++ src/main/java/com/javeriana/proyecto/proyecto/dto/BancoDTO.java
@@ -0,0 +1,17 @@
+package com.javeriana.proyecto.proyecto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BancoDTO {
+
+    private long id;
+    private String nombre;
+
+}
